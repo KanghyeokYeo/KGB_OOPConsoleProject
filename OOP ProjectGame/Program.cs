715b2d2fdbdf431dde5c9b574bc55fbb0530f4db
KanghyeokYeo <ykh5548@naver.com>
2024-08-13 17:14:20 +0900

    프로젝트 생성
    공격, 회피 인터페이스 생성

---
+++ OOP ProjectGame/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP_ProjectGame
+{
+    internal class Program
+    {
+        public interface IAttack 
+        {
+            public void Jap();
+            public void Straight();
+            public void LeftHook();
+            public void RightHook();
+            public void LeftUpper();
+            public void RightUpper();
+            public void Leftbody();
+            public void Rightbody();
+        }
+        public interface IAvoid 
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
