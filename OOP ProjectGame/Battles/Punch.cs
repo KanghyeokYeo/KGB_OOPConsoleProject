4cabb51be58b0e9e8cdd2a7464d73b6afe1eae62
KanghyeokYeo <ykh5548@naver.com>
2024-08-16 17:16:24 +0900

    Punch에 주석
    의도 설명

--- OOP ProjectGame/Battles/Punch.cs
+++ OOP ProjectGame/Battles/Punch.cs
@@ -13,6 +13,7 @@ namespace OOP_ProjectGame.Battles
         public Punch(Game game) : base(game) { }
         public override void Enter()
         {
+            // 정해진 시간 내에 입력받지 못한 경우 회피 하지 못한 판정으로 하려 함
             Console.Clear();
             Console.WriteLine("Punch가 날라옵니다");
             Console.WriteLine("회피를 하려면 1을 입력하시오");
@@ -31,6 +32,7 @@ namespace OOP_ProjectGame.Battles
         }
         public override void Render()
         {
+            // 회피에 성공하여 반격하려 함
             Console.Clear();
             Console.WriteLine("반격할 공격을 선택하시오");
             Console.WriteLine("1. : Hook");
@@ -54,6 +56,7 @@ namespace OOP_ProjectGame.Battles
                     game.enemy.curHp -= 20;
                     game.ChangeScene((BattleType)BattleType.Sellect);
                     break;
+                    // 정해진 시간 내에 반격하지 않은 경우 다시 자리 잡음
                 default:
                     Console.Clear();
                     Console.WriteLine("상대가 다시 자세를 잡았습니다");
